8943e3809834d81e6f2e10be5a4e1dc78638cf08
Patrick Frendo <patar450@gmail.com>
2023-02-27 00:05:02 +0100

    Revert "Implementation of duplicate code mitigation."
    This reverts commit b981ff584c2a852bbe86a8d483c15d908cd0a767.

--- my-app/src/api/authentication.php
+++ my-app/src/api/authentication.php
@@ -1,6 +1,18 @@
 <?php
 
-require("./databasecon.php");
+//Details of the Database.
+$servername = "localhost";
+$username = "id20324296_tester";
+$password = "0123456789abc-A";
+$dbname = "id20324296_test";
+
+// Create a new connection and assign to $conn variable
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection exists, if not then terminate it and show an error message.
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 // Trim the variables of any unneccasary spaces. 
 function postCleanForEmail($value)

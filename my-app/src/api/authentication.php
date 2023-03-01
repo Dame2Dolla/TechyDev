066a390be1f8daee44bf91d11048bb908feec1e1
Patrick Frendo <patar450@gmail.com>
2023-03-01 20:56:45 +0100

    Require php function
    Managed to make the Require function work. This allows us to remove duplicate data and thus increase our code security.

--- my-app/src/api/authentication.php
+++ my-app/src/api/authentication.php
@@ -1,20 +1,6 @@
 <?php
-//Error reporting function that disabled error messages.
-error_reporting(0);
-session_start();
-//Details of the Database.
-$servername = "localhost";
-$username = "id20324296_tester";
-$password = "0123456789abc-A";
-$dbname = "id20324296_test";
 
-// Create a new connection and assign to $conn variable
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection exists, if not then terminate it and show an error message.
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once __DIR__ . '/conn.php';
 
 // Trim the variables of any unneccasary spaces. 
 function postCleanForEmail($value)

933860f6f91f8d2322a8e3ecffff624a9bdc35b4
Patrick Frendo <patar450@gmail.com>
2023-03-04 12:42:44 +0100

    Removed scrapped React Website

--- my-app/React-Project-Scrapped-Do-Not_Touch/src/component/Overlay/Overlay.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Overlay.scss";
-
-const Overlay = (props) => {
-  return <div className="overlay" onClick={props.onClick}></div>;
-};
-
-export default Overlay;

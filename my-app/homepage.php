641678fb12369f85d376f7c8e62848124f18e9bf
Patrick Frendo <patar450@gmail.com>
2023-03-04 17:24:05 +0100

    Edited homepage
    - Added profile pic
    - Texts

--- my-app/homepage.php
+++ my-app/homepage.php
@@ -6,21 +6,45 @@
     <header>
         <div class="header">
             <div class="logo">
-                <img src="./images/icon.png" width="60px" height="60px" alt="Logo">
+                <img src="./images/icon.jpg" width="60px" height="60px" alt="Logo">
             </div>
             <div class="user-info">
-                <!-- <p><?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?></p> -->
-                <p>Patrick Frendo</p>
+                <?php error_reporting(0);
+                echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?>
                 <a class="btn btn-dark" href="logout.php">Log out</a>
             </div>
         </div>
     </header>
     <div class="profile-page">
         <h1>Profile Details</h1>
-        <a class="btn btn-secondary" href="logout.php">Delete Profile</a>
+        <div class="profile-primary-details">
+            <!--Photo by <a href="https://unsplash.com/@icons8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Icons8 Team</a> on <a href="https://unsplash.com/photos/FcLyt7lW5wg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> -->
 
-        <a class="btn btn-dark" href="logout.php">Log out</a>
-    </div>
+            <img src="./images/default_profile.jpg" class="profile-image" width="250px" height="250px"></img>
+
+            <div class="profile-information">
+                <div class="profile-primary-information-firstsection">
+                    <p>Patrick&nbsp;Frendo</p>
+                    <p>Email@email.com</p>
+                    <p>Gender</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="profile-secondary">
+            <div class="profile-secondary-information-Bio">
+                <label>Bio:</label>
+                <textarea placeholder="Bio"></textarea>
+            </div>
+            <div class="change-password-link">
+                <button type="button" class="btn btn-danger">Change Password</button>
+            </div>
+        </div>
+        <div class="profile-buttons">
+            <a class="btn btn-secondary" href="logout.php">Delete Profile</a>
+            <a class="btn btn-dark" href="logout.php">Log out</a>
+
+        </div>
 </body>
 
 </html>

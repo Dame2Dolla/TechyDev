8e5d4a1afe9a28a8277d9be291e7e611a0752bac
Patrick Frendo <Patar450@gmail.com>
2023-04-23 11:47:19 +0200

    404-Error-Page
    - Implemented a 404 custom error page for our student project.

---
+++ my-app/404error.php
@@ -0,0 +1,24 @@
+<?php require "./php_require/htmlheader.php" ?>
+<title>404 Error</title>
+</head>
+
+<body class="backgroundError">
+    <img src="./images/img_404_commet.png" alt="Commet.png" class="commetError" height="100%" width="100%" />
+    <div class="flexError">
+        <div class="subFlexError">
+            <h1 class="headingError textStyle">Oops! <br>Looks like you've wandered into the void.</h1>
+            <p class="pError textStyle">
+                <span>Don't panic! We're on a mission to save you.&nbsp;</span>
+                <span>
+                    <a class="aError" href="/index.php">Click here</a>&nbsp;to get back home.
+                </span>
+            </p>
+            <img src="./images/img_404_rocket.png" alt="Rocket.png" class="rocketError" width="100%" height="100%" />
+        </div>
+        <div>
+            <img src="./images/img_404.png" alt="404Error.png" class="imageError" width="100%" height="100%" />
+        </div>
+    </div>
+</body>
+
+</html>
